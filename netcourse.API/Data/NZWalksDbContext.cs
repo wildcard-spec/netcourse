de27678cc75f97a3fb81c3c9fc223d8b86af2b0f
Danil Samsonov <dsamsonov@deloittece.com>
2023-03-09 13:32:49 +0100

    end of section 2

---
+++ netcourse.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using netcourse.API.Models.Domain;
+
+namespace netcourse.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions <NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties{ get; set; }
+
+
+    }
+}
---
+++ netcourse.API/Migrations/20230309122842_migration 2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace netcourse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Area",
+                table: "Regions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Regions");
+        }
+    }
+}
